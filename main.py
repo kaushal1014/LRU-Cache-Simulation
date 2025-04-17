8e554d97281c4e92c6769ea7bf9c7d3188ea3377
Kaushal Patil <kaushal.patil163@gmail.com>
2025-04-17 19:08:11 +0530

    Added comments

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 
 class LRU:
+    # Initialising the class variables
     def __init__(self,size):
         self.hit=0
         self.miss=0
         self.order=[]
         self.size=size
     
+    #Function for accessing a page, It registers the hit if the page accessed is in the cache else it is a miss.
+    #We use a simple double ended Queue for this.
+    #The least recently used is put in the front and is removed using pop(0) where 0 is the index
+    #The newly Added/Hit element is appended to the end of the queue.
     def access(self,page):
         if page in self.order:
             self.hit+=1
@@ -20,6 +25,7 @@ class LRU:
             self.order.append(page)
             return 0
     
+    #Function to compute the hit and miss rate.
     def stats(self):
         sum=self.hit+self.miss
         if sum>0:
@@ -31,6 +37,8 @@ class LRU:
             )
         else:
             return 0
+        
+    #Function to simulate the cache accessing, it uses the random module to create a random sequence of elements.
     def simulate(self,max):
         for i in range(0,self.size):
             random_page=random.randint(1,max)
@@ -41,8 +49,12 @@ class LRU:
             else:
                 print("MISS")
 
-cache=LRU(50)
+
+#Creating a class and calling the needed function. The parameter is the size of the cache
+cache=LRU(5)
+#Simulateing the accessing of the cache and the parameter is the max element random can choose upto.
 cache.simulate(100)
+#Computing the stats and priting it
 x=cache.stats()
 print(x)
     
