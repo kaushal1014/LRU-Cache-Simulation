73b2d12915ca07994fe153ebdf70028210466454
Kaushal Patil <kaushal.patil163@gmail.com>
2025-04-17 19:01:53 +0530

    Added simulating

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-r=-1;f=-1
-CAHCE_SIZE=5
+import random
 
 class LRU:
     def __init__(self,size):
@@ -13,23 +12,37 @@ class LRU:
             self.hit+=1
             self.order.remove(page)
             self.order.append(page)
+            return 1
         else:
             self.miss+=1;
             if len(self.order)>=self.size:
                 self.order.pop(0)
             self.order.append(page)
+            return 0
     
     def stats(self):
         sum=self.hit+self.miss
         if sum>0:
-            return "Hits:",self.hit,"\n","Hit Rate:",self.hit/sum,"\n","Misses:",self.miss,"\n","Miss Rate:",self.miss/sum
+            return(
+                f"Hits: {self.hit}\n"
+                f"Misses: {self.miss}\n"
+                f"Hit rate: {self.hit}/{sum}\n"
+                f"Miss rate: {self.miss}/{sum}"
+            )
         else:
             return 0
+    def simulate(self,max):
+        for i in range(0,self.size):
+            random_page=random.randint(1,max)
+            print("Accessing page:",random_page)
+            y=self.access(random_page)
+            if y==1:
+                print("HIT")
+            else:
+                print("MISS")
 
-#testing
-chinNIG=LRU(5)
-list=[1,1]
-
-for i in list:
-    chinNIG.access(i)
+cache=LRU(50)
+cache.simulate(100)
+x=cache.stats()
+print(x)
     
